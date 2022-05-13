b6ec1a5fee0c55e62ad72c8b3129771319d2d1ec
Your Name <you@example.com>
2022-05-13 16:35:48 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import Card from './components/Card';
+import Header from './components/Header';
+import Drawer from './components/Drawer';
+
+function App() {
+  return (
+    <div className="wrapper  clear">
+      <Drawer />
+      <Header />
+
+      <div className="content p-40">
+        <div className="d-flex justify-between mb-40 align-center">
+          <h1 >Все кроссовки</h1>
+          <div className="search-block d-flex">
+            <img src="img/search.svg" alt="Search" />
+            <input placeholder="Поиск..." type="text" />
+          </div>
+        </div>
+        <div className="d-flex">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
